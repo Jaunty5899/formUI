5e5dc2b1e181a1e1d39afddad34c15bea2976aa0
Jaunty <jaunty5899@gmail.com>
2024-12-16 14:31:20 +0530

    sub section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container">
+      <form className="subDiv1" action="/">
+        <label htmlFor="fullName">Full Name</label>
+        <input
+          id="fullName"
+          type="text"
+          placeholder="What should we call you?"
+        />
+        <label htmlFor="email">Email</label>
+        <input id="email" type="email" placeholder="Email" required />
+        <label htmlFor="password">Password</label>
+        <input id="password" type="password" placeholder="XXXXXXXX" required>
+          asd
+        </input>
+        <label htmlFor="confirmPassword">Confirm Password</label>
+        <input
+          id="confirmPassword"
+          type="password"
+          placeholder="XXXXXXXX"
+          required
+        />
+        <button>Update Profile</button>
+      </form>
+      <div className="subDiv2"></div>
+    </div>
+  );
 }
 
-export default App
+export default App;
